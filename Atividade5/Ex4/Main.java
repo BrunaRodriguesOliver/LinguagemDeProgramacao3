a185d043451ff8a0d6baa515a3f25b8211673348
BrunaRodriguesOliver <58796557+BrunaRodriguesOliver@users.noreply.github.com>
2021-07-07 18:20:48 -0300

    Add files via upload

---
+++ Atividade5/Ex4/Main.java
@@ -0,0 +1,15 @@
+package Ex4;
+
+import java.util.stream.Stream;
+import javax.naming.NameNotFoundException;
+
+public class Main {
+
+	public static void main(String[] args) throws NameNotFoundException {
+		
+		Stream.of("Paulo", "Camila", "Ana Maria", "Patrick", "Ana Clara", "Pedro", "Alfredo")
+		.filter(n -> n.startsWith("W")).findAny().orElseThrow(()-> new NameNotFoundException());
+
+	}
+
+}
